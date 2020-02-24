34259beedbdd279e28b70e177e8c9ae7c321e0d6
Glenn Ko <glidias@hotmail.com>
2020-02-25 00:37:03 +0800

    remove unneeded babel polyfill for typescript compile

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,3 @@
-// !!!REQUIRED IMPORTS!!!
-import "babel-polyfill";
-
-
-
-/**********************************************/
-/* Your code                                  */
-/**********************************************/
-
 import './example';
 //import './example.js';
 console.log("Main.js detected: runtime? "+ (typeof window === 'object' && typeof importScripts !== 'function'));
