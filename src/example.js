bf2e1090db2ea6be4628130a4ca212a5625ff533
Glenn Ko <glidias@hotmail.com>
2020-02-25 22:58:18 +0800

    aha, simple as jsdoc

--- src/example.js
+++ src/example.js
@@ -1,20 +1,14 @@
 //See reference project https://playcanvas.com/project/504134/overview/test-es6
-
-const pc = require("playcanvas");
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
-
+/**
+ * @type {typeof pc.ScriptType}
+ */
 const Example = pc.createScript('example');
 
-/**
- * Hmm.. augments won't work though for intellisense.
- * https://github.com/microsoft/TypeScript/issues/30943
- * @augments pc.ScriptType
- */
+
 Example.prototype.construct = function () {
-    /** @type {pc.ScriptType} */ const the = this;
-    // using "the" instead of "this" for intellisense? Lol.
-    alert('a');
+    
 }
 
 //Support for async functions
